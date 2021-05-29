0c6a2841dd55ead75d607d8f13f3f6d51f9ff164
anrodriguez138 <anrodriguez138@poligran.edu.co>
2021-05-28 22:59:24 -0500

    Ajuste extencion, php, geo

--- Operaciones_Basicas.php
+++ Operaciones_Basicas.php
@@ -216,12 +216,12 @@ if(!isset($_SESSION['user'])){
                 <div class="cont-expert">
                     <img src="img/Geometria.jpg" alt="">
                     <h3 class="n-expert"></h3>
-                    <a href ="Geometria.html">Geometria</a>.
+                    <a href ="Geometria.php">Geometria</a>.
                 </div>
                 <div class="cont-expert">
                     <img src="img/Conjunto.jpg" alt="">
                     <h3 class="n-expert"></h3>
-                    <a href ="Conjuntos.html">Conjuntos</a>.
+                    <a href ="Conjuntos.php">Conjuntos</a>.
                 </div>
             </section>
         </section>
